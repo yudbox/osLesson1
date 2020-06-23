e1a7eceb56527c6ca9ac7c034c077bbff11b2d5d
Yudbox <yudbox4work@gmail.com>
2020-06-23 12:38:01 +0300

    Updates

---
+++ precache-manifest.e80387f3a20c2780047264a6d0286fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f755a7903ed23f62f2b12748e4a4a8b0",
+    "url": "/yudbox/osLesson1/index.html"
+  },
+  {
+    "revision": "a63bef1dadda5f7109e5",
+    "url": "/yudbox/osLesson1/static/css/main.aa3664a4.chunk.css"
+  },
+  {
+    "revision": "6add773f07f001c332f2",
+    "url": "/yudbox/osLesson1/static/js/2.33807f87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yudbox/osLesson1/static/js/2.33807f87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63bef1dadda5f7109e5",
+    "url": "/yudbox/osLesson1/static/js/main.5156c683.chunk.js"
+  },
+  {
+    "revision": "cd59e917afb67a817109",
+    "url": "/yudbox/osLesson1/static/js/runtime-main.f79c74a1.js"
+  }
+]);
