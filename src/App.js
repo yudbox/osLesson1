ae59a9a2a1f575126e58a198aee2587ef974ffbc
Yudbox <yudbox4work@gmail.com>
2020-06-23 15:08:47 +0300

    Add Hash router for Github correct work

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import './App.css';
-import { connect } from 'react-redux';
-import { compose } from 'redux'
-import { Route, withRouter, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, HashRouter } from 'react-router-dom';
 import Sidebar from './components/Sidebar/Sidebar'
 import Chessboard from './components/Chessboard/Chessboard'
 import CatalogContainer from './components/Catalog/CatalogContainer'
 import GoodsItemContainer from './components/GoodsItem/GoodsItemContainer'
 import PrivatRoomContainer from './components/PrivatRoom/PrivatRoomContainer'
+import { Provider } from 'react-redux';
+import { compose } from 'redux'
 
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="body-container">
-        <Sidebar />
-        <div className="main-content">
-          <Route path="/chessboard" render={() => <Chessboard />} />
-          <Route path="/catalog" render={() => <CatalogContainer />} />
-          <Route path="/goodsitem/:goodsId" render={() => <GoodsItemContainer />} />
-          <Route path="/privatroom" render={() => <PrivatRoomContainer />} />
-        </div>
+    <div className="body-container">
+      <Sidebar />
+      <div className="main-content">
+        <Route path="/chessboard" render={() => <Chessboard />} />
+        <Route path="/catalog" render={() => <CatalogContainer />} />
+        <Route path="/goodsitem/:goodsId" render={() => <GoodsItemContainer />} />
+        <Route path="/privatroom" render={() => <PrivatRoomContainer />} />
       </div>
-    </BrowserRouter>
+    </div>
   )
 }
 
-export default App;
+
+
+
+const AppContainer = (props) => {
+  return <HashRouter basename={process.env.PUBLIC_URL}>
+    <Provider store={props.store}>
+      <App />
+    </Provider>
+  </HashRouter>
+}
+
+export default AppContainer;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppContainer from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <AppContainer store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
