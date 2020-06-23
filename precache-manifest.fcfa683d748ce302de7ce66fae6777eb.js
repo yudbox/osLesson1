9072ddeaf01279870718ec8e22925df4c5c5f729
Yudbox <yudbox4work@gmail.com>
2020-06-23 14:10:11 +0300

    Updates

---
+++ precache-manifest.fcfa683d748ce302de7ce66fae6777eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2192b275093a2d5850",
+    "url": "./2.b6f20b62.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./2.b6f20b62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e776a5190e673b693bfe8180b606562f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fdd7f035ac7e1dbf32c",
+    "url": "./main.aa3664a4.chunk.css"
+  },
+  {
+    "revision": "3fdd7f035ac7e1dbf32c",
+    "url": "./main.f893719a.chunk.js"
+  },
+  {
+    "revision": "bf0ed9d08549c812e55f",
+    "url": "./runtime-main.e6ae2e8b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.905046834fd49cc2f5860273b917c331.js"
+  "./precache-manifest.fcfa683d748ce302de7ce66fae6777eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
